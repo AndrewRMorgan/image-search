3a88eed448df2187cf3aea144fe4a4d69035606b
AndrewRMorgan <andrew.r.morgan@gmail.com>
2017-05-08 18:11:56 +1000

    add function for config file

--- main.go
+++ main.go
@@ -1,35 +1,59 @@
 package main
 
 import (
-  "net/http"
-  "encoding/json"
-  "fmt"
-  "os"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
 
-  "github.com/julienschmidt/httprouter"
+	"github.com/julienschmidt/httprouter"
 )
 
-func main() {
-  port := os.Getenv("PORT")
-  if port == "" {
-    port = "8080"
-  }
+type ImageJson struct {
+	URL       string `json:"url"`
+	Snippet   string `json:"snippet"`
+	Thumbnail string `json:"thumbnail"`
+	Context   string `json:"context"`
+}
 
-  router := httprouter.New()
-  router.GET("/api/imagesearch/:queries", getQuery)
-  router.GET("/api/latest/imagesearch", getLatest)
-  router.GET("/", index)
-  http.ListenAndServe(":"+port, router)
+type Config struct {
+	API string
+	Cx string
+}
+
+file, _ := os.Open("config.json")
+decoder := json.NewDecoder(file)
+config := Config{}
+err := decoder.Decode(&config)
+check(err)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	router := httprouter.New()
+	router.GET("/api/imagesearch/:queries", getQuery)
+	router.GET("/api/latest/imagesearch", getLatest)
+	router.GET("/", index)
+	http.ListenAndServe(":"+port, router)
 }
 
 func getQuery(res http.ResponseWriter, req http.Request, ps httprouter.Params) {
+  query := ps.ByName("queries")
+}
+
+func getLatest(res http.ResponseWriter, req http.Request, _ httprouter.Params) {
 
 }
 
-func getLatest(res http.ResponseWriter, req http.Request, ps httprouter.Params) {
-
+func index(res http.ResponseWriter, req http.Request, _ httprouter.Params) {
+  http.ServeFile(res, req, "./static/index.html")
 }
 
-func index(res http.ResponseWriter, req http.Request, ps httprouter.Params) {
-
+func check(err error) {
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
 }
